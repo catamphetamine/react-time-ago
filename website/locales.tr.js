dd181f10e35644de7ace21fea9bf1bcfb97466f9
AlwahaL <44875945+AlwahaL@users.noreply.github.com>
2021-12-17 11:59:58 +0300

    Create locales.tr.js

---
+++ website/locales.tr.js
@@ -0,0 +1,226 @@
+var long = {
+  "year": {
+    "future": {
+      "one": "{0} yıl içerisinde",
+      "other": "{0} yıl içerisinde"
+    },
+    "past": {
+      "one": "{0} yıl önce",
+      "other": "{0} yıl önce"
+    }
+  },
+  "month": {
+    "future": {
+      "one": "{0} ay içerisinde",
+      "other": "{0} ay içerisinde"
+    },
+    "past": {
+      "one": "{0} ay önce",
+      "other": "{0} ay önce"
+    }
+  },
+  "week": {
+    "future": {
+      "one": "{0} hafta içerisinde",
+      "other": "{0} hafta içerisinde"
+    },
+    "past": {
+      "one": "{0} hafta önce",
+      "other": "{0} hafta önce"
+    }
+  },
+  "day": {
+    "future": {
+      "one": "{0} gün içerisinde",
+      "other": "{0} gün içerisinde"
+    },
+    "past": {
+      "one": "{0} gün önce",
+      "other": "{0} gün önce"
+    }
+  },
+  "hour": {
+    "future": {
+      "one": "{0} saat içerisinde",
+      "other": "{0} saat içerisinde"
+    },
+    "past": {
+      "one": "{0} saat önce",
+      "other": "{0} saat önce"
+    }
+  },
+  "minute": {
+    "future": {
+      "one": "{0} dakika içerisinde",
+      "other": "{0} dakika içerisinde"
+    },
+    "past": {
+      "one": "{0} dakika önce",
+      "other": "{0} dakika önce"
+    }
+  },
+  "second": {
+    "future": {
+      "one": "{0} saniye içerisinde",
+      "other": "{0} saniye içerisinde"
+    },
+    "past": {
+      "one": "{0} saniye önce,
+      "other": "{0} saniye önce"
+    }
+  }
+}
+
+var short = {
+  "year": {
+    "future": {
+      "one": "{0} yıl.",
+      "other": "{0} yıl."
+    },
+    "past": {
+      "one": "{0} y. önce",
+      "other": "{0} y. önce"
+    }
+  },
+  "month": {
+    "future": {
+      "one": "{0} ay",
+      "other": "{0} ay"
+    },
+    "past": {
+      "one": "{0} ay önce",
+      "other": "{0} ay önce"
+    }
+  },
+  "week": {
+    "future": {
+      "one": "{0} hft.",
+      "other": "{0} hft."
+    },
+    "past": {
+      "one": "{0} hft. önce",
+      "other": "{0} hft. önce"
+    }
+  },
+  "day": {
+    "previous": "bir gün önce",
+    "next": "bu gün",
+    "future": {
+      "one": "{0} gün içinde",
+      "other": "{0} gün içinde"
+    },
+    "past": {
+      "one": "{0} gün önce",
+      "other": "{0} gün önce"
+    }
+  },
+  "hour": {
+    "future": {
+      "one": "{0} saat.",
+      "other": "{0} saat."
+    },
+    "past": {
+      "one": "{0} saat önce",
+      "other": "{0} saat önce"
+    }
+  },
+  "minute": {
+    "future": {
+      "one": "{0} dak.",
+      "other": "{0} dak."
+    },
+    "past": {
+      "one": "{0} dak. önce",
+      "other": "{0} dak. agönceo"
+    }
+  },
+  "second": {
+    "past": {
+      "one": "{0} san. önce",
+      "other": "{0} san. önce"
+    },
+    "future": {
+      "one": "{0} san.",
+      "other": "{0} san."
+    }
+  },
+  "now": {
+    "past": "şimdi",
+    "future": "şimdi"
+  }
+}
+
+var mini = {
+  "year": {
+    "future": {
+      "other": "{0}yıl"
+    },
+    "past": {
+      "other": "{0}yıl"
+    }
+  },
+  "month": {
+    "future": {
+      "other": "{0}ay"
+    },
+    "past": {
+      "other": "{0}ay"
+    }
+  },
+  "week": {
+    "future": {
+      "other": "{0}hft"
+    },
+    "past": {
+      "other": "{0}hft"
+    }
+  },
+  "day": {
+    "future": {
+      "other": "{0}gn"
+    },
+    "past": {
+      "other": "{0}gn"
+    }
+  },
+  "hour": {
+    "future": {
+      "other": "{0}saat"
+    },
+    "past": {
+      "other": "{0}saat"
+    }
+  },
+  "minute": {
+    "future": {
+      "other": "{0}dak"
+    },
+    "past": {
+      "other": "{0}dak"
+    }
+  },
+  "second": {
+    "past": {
+      "other": "{0}sn"
+    },
+    "future": {
+      "other": "{0}sn"
+    }
+  }
+}
+
+var now = {
+  "now": {
+    "current": "şimdi",
+    "future": "bir anda",
+    "past": "hemen şimdi"
+  }
+}
+
+window['javascript-time-ago/locales/tr'] = {
+  locale: 'tr',
+  mini,
+  short,
+  long,
+  now
+}
