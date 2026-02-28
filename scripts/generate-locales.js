b6b44135272d52e4804287817d6601a4eafdc10b
catamphetamine <kuchumovn@gmail.com>
2026-02-28 19:13:52 +0300

    .

--- scripts/generate-locales.js
+++ scripts/generate-locales.js
@@ -20,14 +20,12 @@ function generateLocales() {
 			`./locale/${locale}/index.js`,
 			LOCALE_INDEX_JS
 				.replaceAll('{LOCALE}', locale)
-				.replaceAll('{LOCALE_VARIABLE_NAME}', getLocaleVariableName(locale))
 		)
 
 		fs.writeFileSync(
 			`./locale/${locale}/index.cjs`,
 			LOCALE_INDEX_CJS
 				.replaceAll('{LOCALE}', locale)
-				.replaceAll('{LOCALE_VARIABLE_NAME}', getLocaleVariableName(locale))
 		)
 
 		fs.writeFileSync(
@@ -52,16 +50,16 @@ const LOCALE_INDEX_DTS = ''
 
 const LOCALE_INDEX_JS = `
 import TimeAgo from "javascript-time-ago"
-import {LOCALE_VARIABLE_NAME} from "javascript-time-ago/locale/{LOCALE}"
+import localeData from "javascript-time-ago/locale/{LOCALE}"
 
-TimeAgo.addLocale({LOCALE_VARIABLE_NAME})
+TimeAgo.addLocale(localeData)
 `.trim()
 
 const LOCALE_INDEX_CJS = `
 var TimeAgo = require("javascript-time-ago")
-var {LOCALE_VARIABLE_NAME} = require("javascript-time-ago/locale/{LOCALE}")
+var localeData = require("javascript-time-ago/locale/{LOCALE}")
 
-TimeAgo.addLocale({LOCALE_VARIABLE_NAME})
+TimeAgo.addLocale(localeData)
 `.trim()
 
 const LOCALE_PACKAGE_JSON = `
@@ -81,12 +79,5 @@ const LOCALE_PACKAGE_JSON = `
 	"sideEffects": true
 }
 `.trim()
-
-// Transforms a locale name to a javascript variable name.
-// Example: "zh-Hant-MO" -> "zh_Hant_MO"
-// Example: "en-001" -> "en_001"
-function getLocaleVariableName(locale) {
-	return locale.replace(/[^a-zA-Z_\d]/g, '_')
-}
 
 generateLocales()
